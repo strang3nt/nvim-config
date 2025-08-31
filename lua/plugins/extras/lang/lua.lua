284db6ea2890b0da66f787500c4a79dadba2ade8
strang3nt <alessandro.flori@pm.me>
2025-08-31 23:07:10 +0200

    Added many plugins and customizations
    Added:
    
    - blink-cmp
    - lualine
    - neotree
    - which-key
    
    Added keybinds for telescope buffers and neotree toggle.

--- lua/plugins/extras/lang/lua.lua
+++ lua/plugins/extras/lang/lua.lua
@@ -1,32 +1,29 @@
 return {
 
-	-- Add ``server`` and ``formatter`` to mason
-	{
-		"mason-org/mason-lspconfig.nvim",
-		opts = function(_, opts)
-			vim.list_extend(opts.ensure_installed, {
-				"lua_ls",
-			})
-		end,
-	},
+    {
+        "mason-org/mason-lspconfig.nvim",
+        opts = function(_, opts)
+            vim.list_extend(opts.ensure_installed, {
+                "lua_ls",
+            })
+        end,
+    },
 
+    {
+        "mason-org/mason.nvim",
+        opts = function(_, opts)
+            vim.list_extend(opts.ensure_installed, {
+                "stylua",
+            })
+        end,
+    },
 
-	-- Add ``server`` and ``formatter`` to mason
-	{
-		"mason-org/mason.nvim",
-		opts = function(_, opts)
-			vim.list_extend(opts.ensure_installed, {
-				"stylua",
-			})
-		end,
-	},
-
-	{
-		"stevearc/conform.nvim",
-		opts = {
-			formatters_by_ft = {
-				lua = { "stylua" },
-			},
-		},
-	},
+    {
+        "stevearc/conform.nvim",
+        opts = {
+            formatters_by_ft = {
+                lua = { "stylua" },
+            },
+        },
+    },
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,7 +1,45 @@
 return {
     {
-        'nvim-telescope/telescope.nvim',
-        branch = '0.1.x',
-        dependencies = { 'nvim-lua/plenary.nvim' }
-    }
+        "nvim-telescope/telescope.nvim",
+        branch = "0.1.x",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        keys = {
+            {
+                "<leader>b",
+                function()
+                    require("telescope.builtin").buffers()
+                end,
+                mode = "n",
+                desc = "Find Buffers",
+            },
+
+            {
+                "<c-b>",
+                function()
+                    require("telescope.builtin").buffers()
+                end,
+                mode = "i",
+                desc = "Find Buffers",
+            },
+        },
+        opts = function()
+            local actions = require("telescope.actions")
+            return {
+                pickers = {
+                    buffers = {
+                        mappings = {
+                            i = {
+                                ["<c-d>"] = actions.delete_buffer
+                                    + actions.move_to_top,
+                            },
+                            n = {
+                                ["dd"] = actions.delete_buffer
+                                    + actions.move_to_top,
+                            },
+                        },
+                    },
+                },
+            }
+        end,
+    },
 }
