d20f7765b55881259318552f6f450c3027211224
Alessandro Flori <aflori@fbk.eu>
2025-10-13 10:17:32 +0200

    Fix mason trigger function

--- lua/plugins/extras/lang/cpp.lua
+++ lua/plugins/extras/lang/cpp.lua
@@ -1,39 +1,38 @@
 return {
 
-  {
-    "mason-org/mason-lspconfig.nvim",
-    opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, {
-        "clangd",
-      })
-    end,
-  },
-
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, {
-        "cpp",
-      })
-    end,
-  },
-
-  {
-    "mason-org/mason.nvim",
-    opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, {
-        "clang-format",
-      })
-    end,
-  },
-
-  {
-    "stevearc/conform.nvim",
-    opts = {
-      formatters_by_ft = {
-        cpp = { "clang-format" },
-      },
+    {
+        "mason-org/mason-lspconfig.nvim",
+        opts = function(_, opts)
+            vim.list_extend(opts.ensure_installed, {
+                "clangd",
+            })
+        end,
     },
-  },
 
+    {
+        "nvim-treesitter/nvim-treesitter",
+        opts = function(_, opts)
+            vim.list_extend(opts.ensure_installed, {
+                "cpp",
+            })
+        end,
+    },
+
+    {
+        "mason-org/mason.nvim",
+        opts = function(_, opts)
+            vim.list_extend(opts.ensure_installed, {
+                "clang-format",
+            })
+        end,
+    },
+
+    {
+        "stevearc/conform.nvim",
+        opts = {
+            formatters_by_ft = {
+                cpp = { "clang-format" },
+            },
+        },
+    },
 }
